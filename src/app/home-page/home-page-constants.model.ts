8f92c200ce0e5cbe50d6264b40c4b5d55eaf5d9f
Luke Pinto <lap5508@gmail.com>
2019-02-09 18:21:17 -0500

    Home Page V1

---
+++ src/app/home-page/home-page-constants.model.ts
@@ -0,0 +1,9 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class HomePageConstants {
+  THREE_SWEET_GIRLS_IMAGE_HEADER = 'Three Sweet Girls';
+  THREE_SWEET_GIRLS_IMAGE_HEADER_DESCRIPTION = 'Artisan Soap';
+}
+
+
